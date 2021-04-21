195f78de86c4ea6f904916673728b332d264835b
thejaynesh <50999288+thejaynesh@users.noreply.github.com>
2021-04-21 12:43:20 +0530

    Update home_manager.php

--- home_manager.php
+++ home_manager.php
@@ -51,7 +51,7 @@
 	<div class="container agile-banner_nav">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-			<h1><a class="navbar-brand" href="home_manager.php">NITC <span class="display"></span></a></h1>
+			<h1><a class="navbar-brand" href="home_manager.php">HMS <span class="display"></span></a></h1>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 			</button>
@@ -208,7 +208,7 @@
 <footer class="py-5">
 	<div class="container py-md-5">
 		<div class="footer-logo mb-5 text-center">
-			<a class="navbar-brand"  href="http://www.nitc.ac.in/" target="_blank">NIT<span class="display"> CALICUT</span></a>
+			<a class="navbar-brand"  href="home.php" target="_blank">HMS<span class="display"></span></a>
 		</div>
 		<div class="footer-grid">
 			
