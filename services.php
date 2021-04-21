09fe65851edef95cb69fb086c929650b964d6ded
thejaynesh <50999288+thejaynesh@users.noreply.github.com>
2021-04-21 12:43:27 +0530

    Update services.php

--- services.php
+++ services.php
@@ -1,18 +1,16 @@
-<!--
-	Author: W3layouts
-	Author URL: http://w3layouts.com
-	License: Creative Commons Attribution 3.0 Unported
-	License URL: http://creativecommons.org/licenses/by/3.0/
--->
+
 
 <?php
 
  require 'includes/config.inc.php';
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title> Intrend Interior Category Flat Bootstrap Responsive Website Template | Services : W3layouts</title>
+<title> Hostel Management System </title>
 	
 	<!-- Meta tag Keywords -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -52,7 +50,7 @@
 		<div class="container agile-banner_nav">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
 				
-				<h1><a class="navbar-brand" href="home.php">NITC<span class="display"></span></a></h1>
+				<h1><a class="navbar-brand" href="home.php">HMS<span class="display"></span></a></h1>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 				</button>
@@ -102,72 +100,32 @@
 	<div class="container py-lg-5 py-3">
 		<h2 class="heading text-capitalize mb-sm-5 mb-4"> Hostels </h2>
 		<div class="row service-grids">
-			<div class="col-lg-4 col-md-6 service-grid1">
-				<a href="application_form.php"><h3>A Hostel</h3></a>
+		<?php
+    
+    $query = "SELECT * FROM Hostel";
+    $result = mysqli_query($conn,$query);
+
+    while ($row = mysqli_fetch_assoc($result)){  
+    	$hostel_name = $row['Hostel_name'];
+		$built_in = $row['built_in'];
+          ?> 
+		  <div class="col-lg-4 col-md-6 service-grid1">
+		  <br>
+				<a href="application_form.php"><h3><?php echo $hostel_name; ?></h3></a>
 				<div class="row">
 					<div class="col-md-3 col-2">
-						<h4>1 yr</h4>
+						<h4><?php echo $built_in; ?></h4>
 					</div>
 					<div class="col-md-9 col-10">
-						<p>A Hostel</p>
+						<p><?php echo $hostel_name; ?></p>
 					</div>
 				</div>
 			</div>
-			<div class="col-lg-4 col-md-6 mt-md-0 mt-5 service-grid1">
-				<h3>B Hostel</h3>
-				<div class="row">
-					<div class="col-md-3 col-2">
-						<h4>3 yr</h4>
-					</div>
-					<div class="col-md-9 col-10">
-						<p>B Hostel</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-4 col-md-6 mt-lg-0 mt-5 service-grid1">
-				<h3>C Hostel</h3>
-				<div class="row">
-					<div class="col-md-3 col-2">
-						<h4>2 yr</h4>
-					</div>
-					<div class="col-md-9 col-10">
-						<p>C Hostel</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-4 col-md-6 mt-5 service-grid1">
-				<h3>D Hostel</h3>
-				<div class="row">
-					<div class="col-md-3 col-2">
-						<h4>4 yr</h4>
-					</div>
-					<div class="col-md-9 col-10">
-						<p>D Hostel</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-4 col-md-6 mt-5 service-grid1">
-				<h3>E Hostel</h3>
-				<div class="row">
-					<div class="col-md-3 col-2">
-						<h4>4 yr</h4>
-					</div>
-					<div class="col-md-9 col-10">
-						<p>E Hostel</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-4 col-md-6 mt-5 service-grid1">
-				<h3>F Hostel</h3>
-				<div class="row">
-					<div class="col-md-3 col-2">
-						<h4>4 yr</h4>
-					</div>
-					<div class="col-md-9 col-10">
-						<p>F Hostel</p>
-					</div>
-				</div>
-			</div>
+<br><br>
+             
+    <?php
+    } 
+?>
 		</div>
 	</div>
 </section>
@@ -178,56 +136,19 @@
 		<div class="container-fluid">
 			<div class="banner_bottom_agile_grids">
 				<div class="row wthree_banner_bottom_right_grids pr-sm-3">
-					<div class="col-lg-3 col-sm-6 banner_bottom_right_grid">
-						<div class="view view-tenth">
-							<div class="agile_text_box">
-								<i class="fas fa-bed" aria-hidden="true"></i>
-								<h3> Apply for A-Hostel</h3>
-								<p>A Hostel</p>
-							</div>
-							<div class="mask">
-								<a href="application_form.php?id=A"><img src="web_home/images/s1.jpg" class="img-responsive" alt="" /></a>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-sm-6 mt-sm-0 mt-3 banner_bottom_right_grid">
-						<div class="view view1 view-tenth">
-							<div class="agile_text_box">
-								<i class="fas fa-bed" aria-hidden="true"></i>
-								<h3>Apply for B-Hostel</h3>
-								<p>B Hostel</p>
-							</div>
-							<div class="mask">
-								<a href="application_form.php?id=B"><img src="web_home/images/s2.jpg" class="img-responsive" alt="" /></a>
-							</div>
-						</div>
-					</div>
-				
-					<div class="col-lg-3 col-sm-6 mt-lg-0 mt-3 banner_bottom_right_grid">
-						<div class="view view2 view-tenth">
-							<div class="agile_text_box">
-								<i class="fas fa-bed" aria-hidden="true"></i>
-								<h3>Apply for C-Hostel</h3>
-								<p>C Hostel</p>
-							</div>
-							<div class="mask">
-								<a href="application_form.php?id=C"><img src="web_home/images/s3.jpg" class="img-responsive" alt="" /></a>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-sm-6 mt-lg-0 mt-3 banner_bottom_right_grid">
-						<div class="view view3 view-tenth">
-							<div class="agile_text_box">
-								<i class="fas fa-bed" aria-hidden="true"></i>
-								<h3>Apply for D-Hostel</h3>
-								<p>D Hostel</p>
-							</div>
-							<div class="mask">
-								<a href="application_form.php?id=D"><img src="web_home/images/s4.jpg" class="img-responsive" alt="" /></a>
-							</div>
-						</div>
-					</div>
-					<div class="clearfix"> </div>
+				<?php
+    
+    $query = "SELECT * FROM Hostel";
+    $result = mysqli_query($conn,$query);
+	$i=0;
+    while ($row = mysqli_fetch_assoc($result)){  
+    	$hostel_name = $row['Hostel_name'];
+		$built_in = $row['built_in'];
+		$hostel_id = $row['Hostel_id'];
+		if($i%4==0 && $i>0){
+?>
+
+<div class="clearfix"> </div>
 			</div>
 			<div class="clearfix"> </div>
 		</div>
@@ -242,71 +163,35 @@
 		<div class="container-fluid">
 			<div class="banner_bottom_agile_grids">
 				<div class="row wthree_banner_bottom_right_grids pr-sm-3">
-					<!--<div class="col-lg-3 col-sm-6 banner_bottom_right_grid">
+		<?php }
+          ?> 
+		  <div class="col-lg-3 col-sm-6 banner_bottom_right_grid">
 						<div class="view view-tenth">
 							<div class="agile_text_box">
-								<i class="fas fa-bath" aria-hidden="true"></i>
-								<h3> Apply for D-Hostel</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adip. Sed semper sem non commodo egestas. In rutrum enim a neque volutpat aliquet</p>
-							</div>
-							<div class="mask">
-								<img src="web_home/images/s1.jpg" class="img-responsive" alt="" />
-							</div>
-						</div>
-					</div>-->
-					<div class="col-lg-3 col-sm-6 mt-sm-0 mt-3 banner_bottom_right_grid">
-						<div class="view view1 view-tenth">
-							<div class="agile_text_box">
 								<i class="fas fa-bed" aria-hidden="true"></i>
-								<h3> Apply for E-Hostel</h3>
-								<p>E Hostel</p>
+								<h3> Apply for <?php echo $hostel_name; ?></h3>
+								<p><?php echo $hostel_name; ?></p>
 							</div>
 							<div class="mask">
-								<a href="application_form.php?id=E"><img src="web_home/images/s2.jpg" class="img-responsive" alt="" /></a>
+								<a href="application_form.php?id=<?php echo $hostel_id; ?>"><img src="web_home/images/s<?php echo $hostel_id; ?>.jpg" class="img-responsive" alt="" /></a>
 							</div>
 						</div>
 					</div>
-				
-					<div class="col-lg-3 col-sm-6 mt-lg-0 mt-3 banner_bottom_right_grid">
-						<div class="view view2 view-tenth">
-							<div class="agile_text_box">
-								<i class="fas fa-bed" aria-hidden="true"></i>
-								<h3>Apply for F-Hostel </h3>
-								<p>F Hostel</p>
-							</div>
-							<div class="mask">
-								<a href="application_form.php?id=F"><img src="web_home/images/s3.jpg" class="img-responsive" alt="" /></a>
-							</div>
-						</div>
-					</div>
-					<!--<div class="col-lg-3 col-sm-6 mt-lg-0 mt-3 banner_bottom_right_grid">
-						<div class="view view3 view-tenth">
-							<div class="agile_text_box">
-								<i class="fas fa-bed" aria-hidden="true"></i>
-								<h3>Office Chairs</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adip. Sed semper sem non commodo egestas. In rutrum enim a neque volutpat aliquet</p>
-							</div>
-							<div class="mask">
-								<img src="web_home/images/s4.jpg" class="img-responsive" alt="" />
-							</div>
-						</div>
-					</div>-->
-					<div class="clearfix"> </div>
-			</div>
-			<div class="clearfix"> </div>
-		</div>
-	</div>
+<br><br>
+             
+    <?php
+	$i++;
+    } 
+?>
+
+<br>
 </div>
-
-<!-- team -->
-
-<!-- //team -->
-
+</br>	
 <!-- footer -->
 <footer class="py-5">
 	<div class="container py-md-5">
 		<div class="footer-logo mb-5 text-center">
-			<a class="navbar-brand" href="http://www.nitc.ac.in/" target="_blank">NIT <span class="display"> CALICUT</span></a>
+			<a class="navbar-brand" href="home.php" target="_blank">HMS <span class="display"></span></a>
 		</div>
 		<div class="footer-grid">
 			
